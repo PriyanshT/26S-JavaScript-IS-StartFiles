3fdec14c547a9bbda6002ca060d5bdde2a3c8208
Priyansh Thakar <priyanshthakar@gmail.com>
2026-07-12 20:08:36 -0400

    Add files via upload

---
+++ Week13/js/nytimes.js
@@ -0,0 +1,21 @@
+// The URL for the Article Search API at nytimes.com
+// STEP 1: Get your own API key and paste it below…
+// Grab references to all the DOM elements you'll need to access
+// STEP 2: Add a submit event listener for the search form, referencing the fetchResults function as the callback
+
+// Functions
+    // Use preventDefault() to stop the form submitting
+    // STEP 3: Assemble the full URL, according to the API documentation at the New York Times
+    // STEP 4: Use fetch() to pass the URL that we built as a request to the API service, then pass the JSON to the displayResults() function
+
+    // STEP 5: Log to the console the results from the API
+
+    // Clear out the old results…
+    // STEP 6: Create the variable articles to capture the articles from the JSON object
+
+
+            // STEP 7: Look at the console output from the API…
+
+            // STEP 8: Put each article together as an ARTICLE element and append it as a child of the SECTION element in the HTML
+
+// This example adapted from "Third-party APIs" at https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Third_party_APIs
